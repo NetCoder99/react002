4d63a39126de49df6a174263ed260c6a62db346f
vs001 <vs001@cox.net>
2019-11-23 14:04:45 -0700

    Module49

--- src/App.js
+++ src/App.js
@@ -54,12 +54,21 @@ class App extends Component {
 
 
    render () {
-    return (
+    const btnStyle = {
+      backgroundColor: "white",
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
 
+    return (
     <div className="App">
       <h1>Hello from React.</h1>
       <p>And here is a paragraph.</p>
-      <button onClick={() => this.switchNameHandler1('Manny')}>Switch Name</button>
+      <button 
+        style={btnStyle}
+        onClick={() => this.switchNameHandler1('Manny')}>Switch Name</button>
       <Person2 
           name={this.state.persons[0].name}  
           age={this.state.persons[0].age}/>
