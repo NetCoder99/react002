3da35206f040ed5f58ff25604fbe01e0d17977a5
vs001 <vs001@cox.net>
2019-11-25 20:01:12 -0700

    Module 60

--- src/App.js
+++ src/App.js
@@ -22,13 +22,19 @@ class App extends Component {
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event)  => {
+  nameChangedHandler = (event, id)  => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState( {    
-      persons: [
-       { name: 'Max', age: 28 },
-       { name: event.target.value, age: 18 },
-       { name: 'Stephanie', age: 57 }
-      ],
+      persons: persons
     })
 
   }
@@ -49,6 +55,7 @@ class App extends Component {
             return (
               <Person2 
                 click={() => this.deletePersonHandler(index)}
+                changed={(evt) => this.nameChangedHandler(evt, person.id)}
                 name={person.name}
                 age={person.age}
                 key={person.id}  
