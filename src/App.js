33d22ee56dcddb8b5cc187aa645690ffbdaf9179
vs001 <vs001@cox.net>
2019-11-29 11:16:12 -0700

    Module 68

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Radium from 'radium';
+
 import './Person/person.css';
 
 import Person2  from './Person/person2.js';
@@ -52,7 +54,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -78,6 +84,10 @@ class App extends Component {
        </div>
       );   
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'pink',
+        color: 'black'
+      }
       btnText.text = "Hide Names";
     }
 
@@ -103,5 +113,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
